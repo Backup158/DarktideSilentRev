3fc9fd45ae20f809a2a2dad68ed1cd587af4be27
Backup158 <beekoop158@gmail.com>
2025-04-19 15:41:55 -0500

    feat: Added options for what to disable and fixed rev down crash for nonexistent sounds

--- SilentRev/scripts/mods/SilentRev/SilentRev.lua
+++ SilentRev/scripts/mods/SilentRev/SilentRev.lua
@@ -12,19 +12,38 @@ local PlayerCharacterSoundEventAliases = require("scripts/settings/sound/player_
 --#################################
 mod.on_all_mods_loaded = function()
     mod:info("SilentRev v" .. mod.version .. " loaded uwu nya :3")
+    --
+    local debug = mod:get("enable_debug_mode")
+    local disable_rev_up = mod:get("disable_rev_up")
+    local disable_rev_idle = mod:get("disable_rev_up")
+    local disable_rev_down = mod:get("disable_rev_down")
 
-    -- Table of chain weapons to iterate through
-    local chain_weapons = {
-        "chainaxe_p1_m1",
-        "chainaxe_p1_m2",
-        "chainsword_2h_p1_m1",
-        "chainsword_2h_p1_m2",
-        "chainsword_p1_m1",
-        "chainsword_p1_m2",
-    }
-    -- Replacing sound with silence
-    for _, weapon_name in ipairs(chain_weapons) do
-        -- Rev up
-        PlayerCharacterSoundEventAliases["sfx_weapon_up"]["events"][weapon_name] = "wwise/events/weapon/play_weapon_silence"
+    if disable_rev_up  then
+        -- Table of chain weapons to iterate through
+        local chain_weapons = {
+            "chainaxe_p1_m1",
+            "chainaxe_p1_m2",
+            "chainsword_2h_p1_m1",
+            "chainsword_2h_p1_m2",
+            "chainsword_p1_m1",
+            "chainsword_p1_m2",
+        }
+        -- Replacing sound with silence
+        for _, weapon_name in ipairs(chain_weapons) do
+            -- Rev up VRRRRRRRRRRRRRR
+            PlayerCharacterSoundEventAliases["sfx_weapon_up"]["events"][weapon_name] = "wwise/events/weapon/play_weapon_silence" 
+        end
+    end
+    -- Unrev purr
+    if disable_rev_down then 
+        PlayerCharacterSoundEventAliases["weapon_special_end"]["events"]["chainaxe_p1_m1"] = "wwise/events/weapon/play_weapon_silence" 
+        PlayerCharacterSoundEventAliases["weapon_special_end"]["events"]["chainaxe_p1_m2"] = "wwise/events/weapon/play_weapon_silence" 
+    end
+
+    if disable_rev_idle then
+        mod:hook_safe(ChainWeaponEffects, "init", function (self, context, slot, weapon_template, fx_sources)
+            local special_active_fx_source_name = fx_sources._melee_idling
+            self._special_active_fx_source_name = melee_idling_fx_source_name
+        end)
     end
 end
--- SilentRev/scripts/mods/SilentRev/SilentRev_data.lua
+++ SilentRev/scripts/mods/SilentRev/SilentRev_data.lua
@@ -1,6 +1,32 @@
 local mod = get_mod("SilentRev")
+
+local finalWidgets = {
+    {
+        setting_id = "enable_debug_mode",
+		type = "checkbox",
+		default_value = false,
+    },
+}
+local widgetsToggleableToAdd = {"disable_rev_up", "disable_rev_idle", "disable_rev_down" }
+-- Appends a toggleable option for a new widget
+local function addWidget(name)
+    -- Write at (table size) + 1, ie inserting at the tail
+    finalWidgets[#finalWidgets + 1] = {
+        setting_id = name,
+        type = "checkbox",
+        default_value = true,
+    }
+end
+-- Adds a widget for each one in the list of names
+for _, name in pairs(widgetsToggleableToAdd) do
+    addWidget(name)
+end
+
 return {
     name = mod:localize("mod_name"),
     description = mod:localize("mod_description"),
     is_togglable = true,
+    options = {
+		widgets = finalWidgets
+	}
 }
