dc3d41d4d6af8a4796d36a9e9b740630281a00bd
Backup158 <beekoop158@gmail.com>
2025-04-19 14:57:58 -0500

    feat: silenced rev up sound. test to silence active time

--- SilentRev/scripts/mods/SilentRev/SilentRev.lua
+++ SilentRev/scripts/mods/SilentRev/SilentRev.lua
@@ -4,7 +4,8 @@ mod.version = "1.0"
 --#################################
 -- Requirements
 --#################################
-
+local ChainWeaponEffects = require("scripts/extension_systems/visual_loadout/wieldable_slot_scripts/chain_weapon_effects")
+local PlayerCharacterSoundEventAliases = require("scripts/settings/sound/player_character_sound_event_aliases")
 
 --#################################
 -- Hooks
@@ -12,6 +13,22 @@ mod.version = "1.0"
 mod.on_all_mods_loaded = function()
     mod:info("SilentRev v" .. mod.version .. " loaded uwu nya :3")
 
+    -- Table of chain weapons to iterate through
+    local chain_weapons = {
+        "chainaxe_p1_m1",
+        "chainaxe_p1_m2",
+        "chainsword_2h_p1_m1",
+        "chainsword_2h_p1_m2",
+        "chainsword_p1_m1",
+        "chainsword_p1_m2",
+    }
+    -- Replacing sound with silence
+    for _, weapon_name in ipairs(chain_weapons) do
+        -- Rev up
+        PlayerCharacterSoundEventAliases["sfx_weapon_up"]["events"][weapon_name] = "wwise/events/weapon/play_weapon_silence"
+    end
 
-
+    mod:hook_safe(ChainWeaponEffects, "init", function (self, context, slot, weapon_template, fx_sources)
+        local special_active_fx_source_name = fx_sources._melee_idling
+    end)
 end
